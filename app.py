125d0816098162f85e8d901aab00a5ca9174b19a
Andrew Ma <andrew.ma1@cibc.com>
2020-11-21 16:37:33 -0700

    Adding /website endpoint that will return website grocery items with prices for Walmart

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+import os
+import re
 from flask import Flask
 from flask import request
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import os
 
 app = Flask(__name__)
 
 URL = "https://www.walmart.ca/en/grocery/dairy-eggs/N-3798"
-CHROME_DRIVER_PATH = os.path.abspath('./chromedriver')
+CHROME_DRIVER_PATH = os.path.abspath('./chromedriver_mac') #comment this line on windows
+
+# CHROME_DRIVER_PATH = os.path.abspath('./chromedriver_windows.exe') # uncomment line on windows
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--incognito')
@@ -16,50 +19,39 @@ options.add_argument('--headless')
 driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
 driver.get(URL)
 
-page = requests.get(URL)
-#Get HTML associated with URL
-# URL = "https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia"
-
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 #testing endpoint
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/website', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        return {
-            "hi": request.args.get('testing'),
-            "bye": "asdfl;kjzxcbl;"
-        }
+    # if request.method == 'POST':
+    #     return {
+    #         "hi": request.args.get('testing'),
+    #         "bye": "asdfl;kjzxcbl;"
+    #     }
     
+    grocery_items = query_prices()
+    return grocery_items
+
+def query_prices():
     f = open("website-content.html", "w")
-    encoding = 'utf-8'
-    
     f.write(driver.page_source)
 
-    # f.write(page.content.decode(encoding))
-    print(page.content)
-    print(CHROME_DRIVER_PATH)
-    print(driver.page_source)
-    print('printed request')
-    return 'Get request for this endpoint'
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    results = soup.find_all('div', class_='product-details-container')
+    grocery_item_dict = {}
 
+    for result in results:
+        match = re.search(r'<h2 class="thumb-header">([^<]*)', str(result))
+        item_name = match.group(1)
+        match = re.search(r'<span>\$</span>([\d\.]*)', str(result))
 
-# #endpoint for webscraping testing yee yee
-# @app.route('/website', methods=['GET', 'POST'])
-# def yeet():
-#     save_html(r.content, )
-#     if request.method == 'POST':
-#         return {
-#             "hi": request.args.get('testing'),
-#             "bye": "asdfl;kjzxcbl;"
-#         }
-#     return 'Get request for this endpoint!'
+        if match is not None:
+            item_price = match.group(1)
+            grocery_item_dict[item_name] = item_price
 
+    grocery_prices = soup.find_all('span', class_='product-price-analytics')
+    return grocery_item_dict
 
-# def save_html(html, path):
-#     with open(path, 'wb') as f:
-#         f.write(html)
-
-    
